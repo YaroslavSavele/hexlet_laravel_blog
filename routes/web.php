ce15e87e4fcfa63ae5bcfd3e5f2819cb78600335
YaroslavSavele <asis174@mail.ru>
2023-06-17 18:15:13 +0500

    lesson 9 Controller

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', function () {
-    return view('about');
-});
+Route::get('about', [\App\Http\Controllers\PageController::class, 'about']);
+
