a45554fe2f340dacef3ea067e47231aa3ee6c51f
YaroslavSavele <asis174@mail.ru>
2023-06-25 12:04:01 +0500

    lesson 14 Создание

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.app')
 
+@if (session()->has('message'))
+    <div class="alert alert-success">
+        {{ session('message') }}
+    </div>
+@endif
+
 @section('content')
     <h1>Список статей</h1>
     @foreach($articles as $article)
-        <a href="{{route('articles.show', ['id' => $article->id])}}"><h2>{{$article->name}}</h2></a>
+        <a href="{{route('articles.show', $article)}}"><h2>{{$article->name}}</h2></a>
         <div>{{Str::limit($article->body, 200)}}</div>
     @endforeach
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,7 @@ Route::get('/', function () {
 
 Route::get('about', [\App\Http\Controllers\PageController::class, 'about']);
 Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::get('articles/create', [ArticleController::class, 'create'])->name('articles.create');
 Route::get('articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
+Route::post('articles', [ArticleController::class, 'store'])->name('articles.store');
 
