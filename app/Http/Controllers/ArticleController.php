65e870fd52c4657d8e24491b21b06398815ee940
YaroslavSavele <asis174@mail.ru>
2023-07-01 13:23:11 +0500

    lesson 17 Resource routing

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ArticleUpdateRequest;
 use App\Models\Article;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ArticleController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|View
+     */
     public function index()
     {
         $articles = Article::paginate();
@@ -15,20 +24,26 @@ class ArticleController extends Controller
         return view('article.index', compact('articles'));
     }
 
-    public function show($id)
-    {
-        $article = Article::findOrFail($id);
-
-        return view('article.show', compact('article'));
-    }
-
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Application|Factory|View
+     */
     public function create()
     {
         $article = new Article();
+
         return view('article.create', compact('article'));
     }
 
-    public function store(Request $request)
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     * @throws ValidationException
+     */
+    public function store(Request $request): RedirectResponse
     {
         $data = $this->validate($request, [
             'name' => 'required|unique:articles',
@@ -38,20 +53,48 @@ class ArticleController extends Controller
         $article = new Article();
         $article->fill($data);
         $article->save();
-        session()->flash('message', 'Post successfully created.');
+        session()->flash('message', 'Post successfully created');
 
         return redirect()->route('articles.index');
     }
 
-    public function edit($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param Article $article
+     * @return Application|Factory|View
+     */
+    public function show(Article $article): View|Factory|Application
     {
-        $article = Article::findOrFail($id);
+        $article = Article::findOrFail($article->id);
+
+        return view('article.show', compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Article $article
+     * @return Application|Factory|View
+     */
+    public function edit(Article $article): View|Factory|Application
+    {
+        $article = Article::findOrFail($article->id);
+
         return view('article.edit', compact('article'));
     }
 
-    public function update(Request $request, $id)
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Article $article
+     * @return RedirectResponse
+     * @throws ValidationException
+     */
+    public function update(Request $request, Article $article): RedirectResponse
     {
-        $article = Article::findOrFail($id);
+        $article = Article::findOrFail($article->id);
         $data = $this->validate($request, [
             'name' => 'required|unique:articles,name,' . $article->id,
             'body' => 'required|min:100',
@@ -61,14 +104,20 @@ class ArticleController extends Controller
         $article->save();
         session()->flash('message', 'Post successfully updated.');
 
-        return redirect()->route('articles.index');
+        return  redirect()->route('articles.index');
     }
 
-    public function destroy($id)
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Article $article
+     * @return RedirectResponse
+     */
+    public function destroy(Article $article): RedirectResponse
     {
-        $article = Article::find($id);
+        $article = Article::find($article->id);
 
-        if ($article) {
+        if($article) {
             $article->delete();
             session()->flash('message', "Статья $article->name была успешно удалена");
         }
@@ -76,4 +125,3 @@ class ArticleController extends Controller
         return redirect()->route('articles.index');
     }
 }
-
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\OldController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,10 +20,5 @@ Route::get('/', function () {
 });
 
 Route::get('about', [\App\Http\Controllers\PageController::class, 'about']);
-Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
-Route::get('articles/create', [ArticleController::class, 'create'])->name('articles.create');
-Route::get('articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
-Route::post('articles', [ArticleController::class, 'store'])->name('articles.store');
-Route::get('articles/{id}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
-Route::patch('articles/{id}', [ArticleController::class, 'update'])->name('articles.update');
-Route::delete('articles/{id}', [ArticleController::class,'destroy'])->name('articles.destroy');
+
+Route::resource('articles', ArticleController::class);
